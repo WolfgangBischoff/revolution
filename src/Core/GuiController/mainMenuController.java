fba5f20de7161b8bcfe98de9ab2005320cffd1b1
wbischof <wolfgang.bischoff@outlook.at>
2019-09-05 12:11:09 +0200

    added testimage

--- src/Core/GuiController/mainMenuController.java
+++ src/Core/GuiController/mainMenuController.java
@@ -5,12 +5,24 @@ import Core.Interpreter;
 import Core.Simulation;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
 public class mainMenuController
 {
     @FXML
-    private Text actiontarget;
+    private ImageView testimage;
+
+
+    @FXML
+    private void initialize()
+    {
+        Image image = new Image("./img/enviro.jpg");
+        testimage = new ImageView();
+        testimage.setImage(image);
+        //testimage.setCache(true);
+    }
 
     @FXML
     protected void gotoSocietyButton(ActionEvent event)
