8f6281337d6f341d73432b8c0d034c880f50edeb
wbischof <wolfgang.bischoff@outlook.at>
2019-09-06 14:53:44 +0200

    Added Quickstart button

--- src/Core/GuiController/mainMenuController.java
+++ src/Core/GuiController/mainMenuController.java
@@ -1,8 +1,6 @@
 package Core.GuiController;
 
-import Core.GameWindow;
-import Core.Interpreter;
-import Core.Simulation;
+import Core.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
@@ -21,7 +19,6 @@ public class mainMenuController
         Image image = new Image("./img/enviro.jpg");
         testimage = new ImageView();
         testimage.setImage(image);
-        //testimage.setCache(true);
     }
 
     @FXML
@@ -35,4 +32,10 @@ public class mainMenuController
         GameWindow.getSingleton().createNextScene("../fxml/economyOverview.fxml");
     }
 
+    @FXML protected void quickPop(ActionEvent event)
+    {
+        Society.getSociety().populateSociety(Util.NUM_PERS_DEFAULT);
+        Simulation.getSingleton().getEconomy().populateEconomy(Util.DEFAULT_NUM_COMPANIES);
+        Simulation.getSingleton().getEconomy().fillWorkplaces();
+    }
 }
